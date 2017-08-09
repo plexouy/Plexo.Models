0f87f68b77380b12ab392fe0be99268556a84377
Maximo Piva <maximo.piva@gmail.com>
2017-08-09 11:32:00 -0300

    Normalize and Cleanup

---
+++ Session.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace Plexo
+{
+    [DataContract]
+    public class Session
+    {
+        [DataMember]
+        public string Id { get; set; }
+
+        [DataMember]
+        public string Uri { get; set; }
+
+        [DataMember]
+        public long ExpirationUTC { get; set; }
+    }
+}
