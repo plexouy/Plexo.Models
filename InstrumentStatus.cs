04c0769cb9f2ac28d68765dbcc514be044d47d2e
Francisco Vidal <fvidal@plexo.com.uy>
2021-04-20 10:16:30 -0300

    InstrumentStatus

---
+++ InstrumentStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plexo
+{
+    [DataContract]
+    public enum InstrumentStatus
+    {
+        [EnumMember]
+        Created,
+        [EnumMember]
+        Updated,
+        [EnumMember]
+        Deleted
+    }
+}
