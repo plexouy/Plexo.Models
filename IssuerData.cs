0025792d1dbdc25a6531cdb787fab914d085dd83
Francisco Vidal <fvidal@plexo.com.uy>
2023-02-02 10:51:50 -0300

    IssuerData update

--- IssuerData.cs
+++ IssuerData.cs
@@ -12,5 +12,11 @@ namespace Plexo
         public int CommerceId { get; set; }
         [DataMember]
         public Dictionary<FieldType,string> Metadata { get; set; }
+        [DataMember]
+        public int? PaymentFacilitatorCommerceId { get; set; }
+        [DataMember]
+        public int? PaymentFacilitatorId { get; set; }
+        [DataMember]
+        public int? PaymentProcessorId { get; set; }
     }
 }
