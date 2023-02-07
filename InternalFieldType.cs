620d9898fc69c38bd98640018b8c8497f9002f70
Francisco Vidal <fvidal@plexo.com.uy>
2023-02-07 14:38:00 -0300

    InternalFields

---
+++ InternalFieldType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Plexo
+{
+    [DataContract]
+    public enum InternalFieldType
+    {
+        IgnoreCVV,
+        ProviderApiKey,
+    }
+}
